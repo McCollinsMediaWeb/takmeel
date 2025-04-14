d0d40c97d311014bb6218f2e91d6d1ce62d570a6
McCollinsMediaWeb <111267011+McCollinsMediaWeb@users.noreply.github.com>
2025-04-14 17:08:58 +0530

    jjjj

--- components/footerBottom/footerBottom.js
+++ components/footerBottom/footerBottom.js
@@ -10,6 +10,16 @@ export default function FooterBottom() {
                 <div className='col-md-8'>
                 © Takmeel Real Estate Development L.L.C 2025. All Rights Reserved. &nbsp;<a href='https://www.mccollinsmedia.com/' target='_blank'>Designed & Developed By Mccollins Media</a>
                 </div>
+                <div className='col-md-4'>
+                    <div className='socialMediaLinks'>
+                        <div className='socialMediaLinksFlex'>
+                            <a href='#' target='_blank'><span className='ScIcon facebook'>&nbsp;</span></a>
+                            <a href='#' target='_blank'><span className='ScIcon instagram'>&nbsp;</span></a>
+                            <a href='#' target='_blank'><span className='ScIcon tiktok'>&nbsp;</span></a>
+                            <a href='#' target='_blank'><span className='ScIcon youtube'>&nbsp;</span></a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
