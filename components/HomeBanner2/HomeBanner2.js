7bbb23c30bcfeb4be49695a6684e11ab7f19475d
McCollinsMediaWeb <111267011+McCollinsMediaWeb@users.noreply.github.com>
2025-04-15 11:31:57 +0530

    555

--- components/HomeBanner2/HomeBanner2.js
+++ components/HomeBanner2/HomeBanner2.js
@@ -89,7 +89,203 @@ export default function HomeBanner2({ backgroundImage, backgroundImageMobile, te
                                                 <div className='Txt3 nunito-text'>{text3}</div>
                                             </div>
                                             <div className='col-md-4'>
-                                                <Link href="/" className='Link1 hover1'>Explore Property</Link>
+                                                <Link href="/" className='Link2 hover1'>Explore Property</Link>
+                                            </div>
+                                        </div>
+                                    </motion.div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='BannerItem'>
+                        <div className='FtSliderContent'>
+                            <motion.div
+                                animate={{ scale: [1, 1.05, 1] }}
+                                transition={{ duration: 6, repeat: Infinity, ease: "easeInOut" }}
+                                viewport={{ once: true, amount: 0.5 }}
+                            >
+                                {isDesktop && (
+                                    <Image
+                                        src={`/${backgroundImage}`}
+                                        width={1338}
+                                        height={714}
+                                        layout="responsive"
+                                        alt="Takmeel"
+                                    />
+                                )}
+                                {!isDesktop && (
+                                    <Image
+                                        src={`/${backgroundImageMobile}`}
+                                        width={616}
+                                        height={714}
+                                        layout="responsive"
+                                        alt="Takmeel"
+                                    />
+                                )}
+                            </motion.div>
+                            <div className='BannerTextBox'>
+                                <div>
+                                    <motion.div
+                                        initial={{ opacity: 0, y: 30 }}
+                                        whileInView={{ opacity: 1, y: 0 }}
+                                        transition={{ duration: 0.6, ease: "easeOut" }}
+                                        viewport={{ once: true, amount: 0.5 }} // triggers when 50% of it is in view
+                                    >
+                                        <div className='row'>
+                                            <div className='col-md-8'>
+                                                <div className='Txt1 nunito-text'>{text1}</div>
+                                                <div className='Txt2 text-uppercase'>{text2}</div>
+                                                <div className='Txt3 nunito-text'>{text3}</div>
+                                            </div>
+                                            <div className='col-md-4'>
+                                                <Link href="/" className='Link2 hover1'>Explore Property</Link>
+                                            </div>
+                                        </div>
+                                    </motion.div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='BannerItem'>
+                        <div className='FtSliderContent'>
+                            <motion.div
+                                animate={{ scale: [1, 1.05, 1] }}
+                                transition={{ duration: 6, repeat: Infinity, ease: "easeInOut" }}
+                                viewport={{ once: true, amount: 0.5 }}
+                            >
+                                {isDesktop && (
+                                    <Image
+                                        src={`/${backgroundImage}`}
+                                        width={1338}
+                                        height={714}
+                                        layout="responsive"
+                                        alt="Takmeel"
+                                    />
+                                )}
+                                {!isDesktop && (
+                                    <Image
+                                        src={`/${backgroundImageMobile}`}
+                                        width={616}
+                                        height={714}
+                                        layout="responsive"
+                                        alt="Takmeel"
+                                    />
+                                )}
+                            </motion.div>
+                            <div className='BannerTextBox'>
+                                <div>
+                                    <motion.div
+                                        initial={{ opacity: 0, y: 30 }}
+                                        whileInView={{ opacity: 1, y: 0 }}
+                                        transition={{ duration: 0.6, ease: "easeOut" }}
+                                        viewport={{ once: true, amount: 0.5 }} // triggers when 50% of it is in view
+                                    >
+                                        <div className='row'>
+                                            <div className='col-md-8'>
+                                                <div className='Txt1 nunito-text'>{text1}</div>
+                                                <div className='Txt2 text-uppercase'>{text2}</div>
+                                                <div className='Txt3 nunito-text'>{text3}</div>
+                                            </div>
+                                            <div className='col-md-4'>
+                                                <Link href="/" className='Link2 hover1'>Explore Property</Link>
+                                            </div>
+                                        </div>
+                                    </motion.div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='BannerItem'>
+                        <div className='FtSliderContent'>
+                            <motion.div
+                                animate={{ scale: [1, 1.05, 1] }}
+                                transition={{ duration: 6, repeat: Infinity, ease: "easeInOut" }}
+                                viewport={{ once: true, amount: 0.5 }}
+                            >
+                                {isDesktop && (
+                                    <Image
+                                        src={`/${backgroundImage}`}
+                                        width={1338}
+                                        height={714}
+                                        layout="responsive"
+                                        alt="Takmeel"
+                                    />
+                                )}
+                                {!isDesktop && (
+                                    <Image
+                                        src={`/${backgroundImageMobile}`}
+                                        width={616}
+                                        height={714}
+                                        layout="responsive"
+                                        alt="Takmeel"
+                                    />
+                                )}
+                            </motion.div>
+                            <div className='BannerTextBox'>
+                                <div>
+                                    <motion.div
+                                        initial={{ opacity: 0, y: 30 }}
+                                        whileInView={{ opacity: 1, y: 0 }}
+                                        transition={{ duration: 0.6, ease: "easeOut" }}
+                                        viewport={{ once: true, amount: 0.5 }} // triggers when 50% of it is in view
+                                    >
+                                        <div className='row'>
+                                            <div className='col-md-8'>
+                                                <div className='Txt1 nunito-text'>{text1}</div>
+                                                <div className='Txt2 text-uppercase'>{text2}</div>
+                                                <div className='Txt3 nunito-text'>{text3}</div>
+                                            </div>
+                                            <div className='col-md-4'>
+                                                <Link href="/" className='Link2 hover1'>Explore Property</Link>
+                                            </div>
+                                        </div>
+                                    </motion.div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='BannerItem'>
+                        <div className='FtSliderContent'>
+                            <motion.div
+                                animate={{ scale: [1, 1.05, 1] }}
+                                transition={{ duration: 6, repeat: Infinity, ease: "easeInOut" }}
+                                viewport={{ once: true, amount: 0.5 }}
+                            >
+                                {isDesktop && (
+                                    <Image
+                                        src={`/${backgroundImage}`}
+                                        width={1338}
+                                        height={714}
+                                        layout="responsive"
+                                        alt="Takmeel"
+                                    />
+                                )}
+                                {!isDesktop && (
+                                    <Image
+                                        src={`/${backgroundImageMobile}`}
+                                        width={616}
+                                        height={714}
+                                        layout="responsive"
+                                        alt="Takmeel"
+                                    />
+                                )}
+                            </motion.div>
+                            <div className='BannerTextBox'>
+                                <div>
+                                    <motion.div
+                                        initial={{ opacity: 0, y: 30 }}
+                                        whileInView={{ opacity: 1, y: 0 }}
+                                        transition={{ duration: 0.6, ease: "easeOut" }}
+                                        viewport={{ once: true, amount: 0.5 }} // triggers when 50% of it is in view
+                                    >
+                                        <div className='row'>
+                                            <div className='col-md-8'>
+                                                <div className='Txt1 nunito-text'>{text1}</div>
+                                                <div className='Txt2 text-uppercase'>{text2}</div>
+                                                <div className='Txt3 nunito-text'>{text3}</div>
+                                            </div>
+                                            <div className='col-md-4'>
+                                                <Link href="/" className='Link2 hover1'>Explore Property</Link>
                                             </div>
                                         </div>
                                     </motion.div>
