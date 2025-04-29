5c6f0a0ab2fe7dd255ebff80ccce295b15c0feae
sahadMccollins <sahad@mccollinsmedia.com>
2025-04-29 11:54:18 +0530

    map data updated

--- app/detail-page/takmeel-al-barari-view-properties/page.js
+++ app/detail-page/takmeel-al-barari-view-properties/page.js
@@ -18,8 +18,8 @@ export default function TakmeelAlBarariViewPropertiesPage() {
     text1: "Urban Living, Reimagined by Nature",
     text2: "Takmeel Al Barari View",
     text3: "Majan, Dubai",
-    videoPosterDesktop:"Takmeel-Al-Barrari-View/Majan 03.jpg",
-    videoPosterMobile:"Takmeel-Al-Barrari-View/Mobile Majan 03.jpg",
+    videoPosterDesktop: "Takmeel-Al-Barrari-View/Majan 03.jpg",
+    videoPosterMobile: "Takmeel-Al-Barrari-View/Mobile Majan 03.jpg",
     GalleryMedia: [
       // {
       //   type: "video",
@@ -146,8 +146,8 @@ export default function TakmeelAlBarariViewPropertiesPage() {
     ],
     mapData: {
       apiKey: "AIzaSyCBtGUl2z4uc5HX9e7eMmzIHalH2a99Oz4",
-      center: { lat: 25.05079, lng: 55.24659 },
-      zoom:13,
+      center: { lat: 25.09870186724474, lng: 55.31477376221619, place_id: "ChIJ12G_-IllXz4RKnVgYcSWR5M" },
+      zoom: 13,
       styles: [
         {
           "featureType": "water",
@@ -249,16 +249,16 @@ export default function TakmeelAlBarariViewPropertiesPage() {
         }
       ],
       nearByPlaces: [
-        { name: "King's College Hospital London", location: { lat: 25.11347333289849, lng: 55.25492898877556 } },
-        { name: "Dunecrest American School", location: { lat: 25.091190585103302, lng: 55.30647784609949 } },
-        { name: "New Earth Cafe", location: { lat: 25.099380109303485, lng: 55.31540940176637 } },
-        { name: "HideOut Al Barari", location: { lat: 25.099574424113847, lng: 55.31581709752857 } },
-        { name: "Mediterranean Garden", location: { lat: 25.096737397233635, lng: 55.31014154332052 } },
-        { name: "Global Village", location: { lat: 25.07341628157727, lng: 55.308393501238776 } },
-        { name: "Cityland Mall", location: { lat: 25.06747303267066, lng: 55.31393249409508 } },
-        { name: "Dubai Outlet Mall", location: { lat: 25.073070634698052, lng: 55.39959151950052 } },
-        { name: "Dubai International Airport", location: { lat: 25.259490296989462, lng: 55.364666407765895 } },
-        { name: "Dubai Mall", location: { lat: 25.20253968228783, lng: 55.278712622706365 } },
+        { name: "King's College Hospital London", place_id: "ChIJ1XhuJe5pXz4RGo-ENO0K6Cs", location: { lat: 25.11347333289849, lng: 55.25492898877556 } },
+        { name: "Dunecrest American School", place_id: "ChIJ41AzRntvXz4R-R6A87ZBfYY", location: { lat: 25.091190585103302, lng: 55.30647784609949 } },
+        { name: "New Earth Cafe", place_id: "ChIJXzBhucplXz4RxzQ-KfL8szg", location: { lat: 25.099380109303485, lng: 55.31540940176637 } },
+        { name: "HideOut Al Barari", place_id: "ChIJtywQsyZlXz4RVBgWuGoZdIk", location: { lat: 25.099574424113847, lng: 55.31581709752857 } },
+        { name: "Mediterranean Garden", place_id: "ChIJX3ac7GNvXz4R5gXVIZ1T1j4", location: { lat: 25.096737397233635, lng: 55.31014154332052 } },
+        { name: "Global Village", place_id: "ChIJFf7go49vXz4RJ6Q_Tvue2Uc", location: { lat: 25.07341628157727, lng: 55.308393501238776 } },
+        { name: "Cityland Mall", place_id: "ChIJMXnawZFvXz4Rtx_7wq9EQjk", location: { lat: 25.06747303267066, lng: 55.31393249409508 } },
+        { name: "Dubai Outlet Mall", place_id: "ChIJdcaW4FRjXz4RhKLN6dkFOu0", location: { lat: 25.073070634698052, lng: 55.39959151950052 } },
+        { name: "Dubai International Airport", place_id: "ChIJaQ4mkwZdXz4R6e5IegDUleY", location: { lat: 25.259490296989462, lng: 55.364666407765895 } },
+        { name: "Dubai Mall", place_id: "ChIJB1zIKShoXz4RnbaTPPup7aU", location: { lat: 25.20253968228783, lng: 55.278712622706365 } },
       ]
     }
   };
