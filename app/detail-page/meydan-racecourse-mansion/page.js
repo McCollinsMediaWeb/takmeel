6add3478a88f9940783eb4c2e296f66a120fa6c1
McCollinsMediaWeb <111267011+McCollinsMediaWeb@users.noreply.github.com>
2025-04-29 16:52:36 +0530

    sss

--- app/detail-page/meydan-racecourse-mansion/page.js
+++ app/detail-page/meydan-racecourse-mansion/page.js
@@ -308,7 +308,7 @@ export default function MeydanRacecourseMansion() {
             <DetailHero {...detailHeroData} />
             <ProjectDetails1  {...DataProjectDetails1} />
             <ProjectDetails2  {...DataProjectDetails2} />
-            <ProjectDetails3 mainimage1="amn1.jpg" data={DataProjectDetails3} />
+            <ProjectDetails3 mainimage1="ovr7.jpg" data={DataProjectDetails3} />
             <ProjectDetails4  {...DataProjectDetails4} />
             {/* <ProjectDetails5 panoramaimage="/str1.jpg" /> */}
             {/* <ProjectDetails6 planImage="plan1.jpg" /> */}
