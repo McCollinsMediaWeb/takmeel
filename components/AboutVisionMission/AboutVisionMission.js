7218bcfd2578f0ef6f10651dcc7926d9dc3254ce
McCollinsMediaWeb <111267011+McCollinsMediaWeb@users.noreply.github.com>
2025-04-30 18:06:10 +0530

    ff

--- components/AboutVisionMission/AboutVisionMission.js
+++ components/AboutVisionMission/AboutVisionMission.js
@@ -6,7 +6,7 @@ export default function AboutVisionMission() {
 
 
     return (
-        <section className="pd-common VisionMissionBox">
+        <section className="pd-common VisionMissionBox bg2 pdb0">
             <div className="container">
                 <div className="text-center">
                     <motion.div
@@ -21,9 +21,13 @@ export default function AboutVisionMission() {
                         </div>
                         <div className="VmT2 nunito-text">You can rely on our team of seasoned specialists to provide the kind of unmatched service that paves the way to unlock the doors to your dream home in one of the world’s most beautiful and sought-after locations. Our company is dedicated to quality, creativity, and the continuous quest to make your dreams a reality. Every project we take on is a blank slate for us to combine our skills, explore new ideas, and push the boundaries of quality and creativity.</div>
                         <div className="VmT3">Takmeel Vision</div>
-                        <div className="VmT4 nunito-text">To redefine urban living through inspired design and future-focused communities.</div>
+                       <div>
+                       <div className="VmT5 nunito-text">To redefine urban living through inspired design and future-focused communities.</div>
+                       </div>
                         <div className="VmT3">Takmeel Mission</div>
-                        <div className="VmT4 nunito-text">To craft sustainable, smart, and elegant spaces that elevate lifestyles and deliver enduring value.</div>
+                        <div>
+                        <div className="VmT5 nunito-text">To craft sustainable, smart, and elegant spaces that elevate lifestyles and deliver enduring value.</div>
+                        </div>
                         {/* <div>
                             <div className="VmT5 nunito-text">CREATE Iconic and Innovative Projects that Redefine Landmarks</div>
                         </div>
@@ -32,9 +36,15 @@ export default function AboutVisionMission() {
                         </div> */}
 
                         <div className="VmT3">Our Values</div>
-                        <div className="VmT4 nunito-text">We champion innovation and sustainability in every design.</div>
-                        <div className="VmT4 nunito-text">Every community is customer-centric, environmentally conscious, and globally inspired.</div>
-                        <div className="VmT4 nunito-text">Our legacy is built on bold ideas, precise execution, and a relentless pursuit of excellence.</div>
+                        <div>
+                            <div className="VmT5 nunito-text">We champion innovation and sustainability in every design.</div>
+                        </div>
+                       <div>
+                        <div className="VmT5 nunito-text">Every community is customer-centric, environmentally conscious, and globally inspired.</div>
+                       </div>
+                       <div>
+                       <div className="VmT5 nunito-text">Our legacy is built on bold ideas, precise execution, and a relentless pursuit of excellence.</div>
+                       </div>
                     </motion.div>
                 </div>
             </div>
