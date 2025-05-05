26db520acbf4adbb97a362389f42d9b0f235f530
sahadMccollins <sahad@mccollinsmedia.com>
2025-05-05 23:37:21 +0530

    admin css bug fix

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -5,6 +5,7 @@ import { getServerSession } from 'next-auth';
 // import { SessionProvider } from './providers/SessionProvider.js';
 // import { SessionProvider } from 'next-auth/react';
 import ClientLayout from './ClientLayout';
+import styles from './style.module.css';
 
 export default async function AdminLayout({ children }) {
 
@@ -13,7 +14,7 @@ export default async function AdminLayout({ children }) {
     return (
 
         <html lang="en">
-            <body>
+            <body className={styles.customFontFamily} >
                 {/* <SessionProvider session={session}> */}
                 <ClientLayout session={session}>
                     <main>{children}</main>
