ed4db28b1b86299d60c1dee1985a700f54516cbb
McCollinsMediaWeb <111267011+McCollinsMediaWeb@users.noreply.github.com>
2025-05-08 17:17:02 +0530

    sss

--- components/ProjectItem/ProjectItem.js
+++ components/ProjectItem/ProjectItem.js
@@ -435,7 +435,7 @@ export default function ProjectItem({
             }
         }
     };
-    
+
 
     const containerRef = useRef(null)
     const isDesktop = useMediaQuery("(min-width: 960px)")
@@ -461,8 +461,14 @@ export default function ProjectItem({
     }
 
     return (
-        <div className="FullScreenBanner Projects">
-            {/* <motion.div
+        <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, ease: "easeOut" }}
+            viewport={{ once: true, amount: 0.5 }}
+        >
+            <div className="FullScreenBanner Projects">
+                {/* <motion.div
                 animate={{ scale: [1, 1.05, 1] }}
                 transition={{ duration: 10, repeat: Number.POSITIVE_INFINITY, ease: "easeInOut" }}
                 viewport={{ once: true, amount: 0.5 }}
@@ -473,108 +479,104 @@ export default function ProjectItem({
                 )}
             </motion.div> */}
 
-            {backgroundVideo ? (
-                <video
-                    src={`/${backgroundVideo}`}
-                    autoPlay
-                    loop
-                    muted
-                    playsInline
-                    preload="none"
-                    style={{ width: '100%' }}
-                />
-            ) : (
-                <motion.div
-                        initial={{ opacity: 0, y: 50 }}
-                        whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.6, ease: "easeOut" }}
-                        viewport={{ once: true, amount: 0.5 }}
-                    >
-            {isDesktop ? (
-                <Image
-                    src={`/${backgroundImage}`}
-                    width={1338}
-                    height={714}
-                    layout="responsive"
-                    alt="Takmeel"
-                    loading="lazy"
-                />
-            ) : (
-                <Image
-                    src={`/${backgroundImageMobile}`}
-                    width={697}
-                    height={768}
-                    layout="responsive"
-                    alt="Takmeel"
-                    loading="lazy"
-                />
-            )}
-        </motion.div>
-            )}
+                {backgroundVideo ? (
+                    <video
+                        src={`/${backgroundVideo}`}
+                        autoPlay
+                        loop
+                        muted
+                        playsInline
+                        preload="none"
+                        style={{ width: '100%' }}
+                    />
+                ) : (
+                    <div>
+                        {isDesktop ? (
+                            <Image
+                                src={`/${backgroundImage}`}
+                                width={1338}
+                                height={714}
+                                layout="responsive"
+                                alt="Takmeel"
+                                loading="lazy"
+                            />
+                        ) : (
+                            <Image
+                                src={`/${backgroundImageMobile}`}
+                                width={697}
+                                height={768}
+                                layout="responsive"
+                                alt="Takmeel"
+                                loading="lazy"
+                            />
+                        )}
+                    </div>
+                )}
 
 
 
-            <div className="FsBannerContent">
-                <div className="FsBannerContentFlex centerBlock">
-                    <div className="container">
-                        
-                        <motion.div
-                            variants={containerVariants}
-                            initial="hidden"
-                            whileInView="visible"
-                            viewport={{ once: true, amount: 0.5 }}
-                        >
-                            <div>
-                               
-                                <motion.div className="Txt1" variants={itemVariants}>
-                                    {text1}
-                                </motion.div>
-                                {projectStatus && (
+                <div className="FsBannerContent">
+                    <div className="FsBannerContentFlex centerBlock">
+                        <div className="container">
+
+                            <motion.div
+                                variants={containerVariants}
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{ once: true, amount: 0.5 }}
+                            >
+                                <div>
+
                                     <motion.div className="Txt1" variants={itemVariants}>
-                                        <div className="Txt1">
-                                            <span className="PrStatus">{projectStatus}</span>
-                                        </div>
+                                        {text1}
                                     </motion.div>
-                                )}
-                                <motion.div className="Txt2 text-uppercase" variants={itemVariants}>
-                                    {text2}
-                                </motion.div>
-                                <motion.div variants={itemVariants}>
-                                    <motion.div
-                                        className="Txt3 nunito-text"
-                                        animate={{ opacity: [1, 0.3, 1] }}
-                                        transition={{
-                                            duration: 2,
-                                            ease: "easeInOut",
-                                        }}
-                                    >
-                                        {text3}
+                                    {projectStatus && (
+                                        <motion.div className="Txt1" variants={itemVariants}>
+                                            <div className="Txt1">
+                                                <span className="PrStatus">{projectStatus}</span>
+                                            </div>
+                                        </motion.div>
+                                    )}
+                                    <motion.div className="Txt2 text-uppercase" variants={itemVariants}>
+                                        {text2}
                                     </motion.div>
-                                </motion.div>
-                                <motion.div className="" variants={itemVariants}>
-                                    <motion.div
-                                        className="Txt17 nunito-text"
-                                        animate={{ opacity: [1, 0.3, 1] }}
-                                        transition={{
-                                            duration: 2,
-                                            ease: "easeInOut",
-                                        }}
-                                    >
-                                        {tagline}
+                                    <motion.div variants={itemVariants}>
+                                        <motion.div
+                                            className="Txt3 nunito-text"
+                                            animate={{ opacity: [1, 0.3, 1] }}
+                                            transition={{
+                                                duration: 2,
+                                                ease: "easeInOut",
+                                            }}
+                                        >
+                                            {text3}
+                                        </motion.div>
                                     </motion.div>
-                                </motion.div>
+                                    <motion.div className="" variants={itemVariants}>
+                                        <motion.div
+                                            className="Txt17 nunito-text"
+                                            animate={{ opacity: [1, 0.3, 1] }}
+                                            transition={{
+                                                duration: 2,
+                                                ease: "easeInOut",
+                                            }}
+                                        >
+                                            {tagline}
+                                        </motion.div>
+                                    </motion.div>
 
-                                <motion.div variants={itemVariants}>
-                                    <Link href={`/detail-page/${url}`} className="Link1 hover1">
-                                        Explore Property
-                                    </Link>
-                                </motion.div>
-                            </div>
-                        </motion.div>
+                                    <motion.div variants={itemVariants}>
+                                        <Link href={`/detail-page/${url}`} className="Link1 hover1">
+                                            Explore Property
+                                        </Link>
+                                    </motion.div>
+                                </div>
+                            </motion.div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
