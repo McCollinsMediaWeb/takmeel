93a14afbf52e2dd27610a33d79132facbc573144
sahadMccollins <sahad@mccollinsmedia.com>
2025-05-16 18:01:50 +0530

    vision mission image updated

--- components/AboutVisionMission/AboutVisionMission2.js
+++ components/AboutVisionMission/AboutVisionMission2.js
@@ -123,7 +123,8 @@ export default function AboutVisionMission2() {
                                 viewport={{ once: true, amount: 0.5 }}
                             >
                                 <Image
-                                    src="/abt3.jpg"
+                                    // src="/abt3.jpg"
+                                    src="/vision-image.jpg"
                                     width={1000}
                                     height={562}
                                     layout="responsive"
@@ -190,7 +191,8 @@ export default function AboutVisionMission2() {
                                 viewport={{ once: true, amount: 0.5 }}
                             >
                                 <Image
-                                    src="/abt4.jpg"
+                                    // src="/abt4.jpg"
+                                    src="/mission-image.jpg"
                                     width={1000}
                                     height={562}
                                     layout="responsive"
