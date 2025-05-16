4f604b38d7ae3163515bdcce07d15ca9b03e0f82
sahadMccollins <sahad@mccollinsmedia.com>
2025-05-16 12:30:11 +0530

    error fix

--- components/ProjectItem/ProjectItem.js
+++ components/ProjectItem/ProjectItem.js
@@ -6,7 +6,6 @@ import Link from "next/link"
 import Image from "next/image"
 import { usePathname } from "next/navigation"
 import useMediaQuery from "../hooks/useMediaQuery"
-import useInView from "../hooks/useInView"
 
 
 export default function ProjectItem({
