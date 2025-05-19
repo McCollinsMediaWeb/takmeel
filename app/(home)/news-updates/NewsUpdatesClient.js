0c7fd004bd9012eb27bfcd24f29ace42ea091ae4
sahadMccollins <sahad@mccollinsmedia.com>
2025-05-19 11:49:39 +0530

    instagram widget added

--- app/(home)/news-updates/NewsUpdatesClient.js
+++ app/(home)/news-updates/NewsUpdatesClient.js
@@ -10,12 +10,20 @@ export default function NewsAndUpdatesClient() {
     // const rest = news?.slice(1);
 
     return (
-        <section className="BlogPostWrap">
-            {/* {latest && <LatestBlog blog={latest} />} */}
-            {/* {rest && rest.length > 0 && <BlogListing blogs={rest} />} */}
-            <LatestBlog/>
-            {news && news.length > 0 && <BlogListing blogs={news} />}
-        </section>
+        <>
+            <section className="BlogPostWrap">
+                {/* {latest && <LatestBlog blog={latest} />} */}
+                {/* {rest && rest.length > 0 && <BlogListing blogs={rest} />} */}
+                <LatestBlog />
+                {news && news.length > 0 && <BlogListing blogs={news} />}
+            </section>
+            <section className="BlogPostWrap">
+                <div className="container">
+                    <script src="https://static.elfsight.com/platform/platform.js" async></script>
+                    <div class="elfsight-app-7659e170-6a8a-44ef-8efb-c398b7349c05" data-elfsight-app-lazy></div>
+                </div>
+            </section>
+        </>
 
     );
 }
