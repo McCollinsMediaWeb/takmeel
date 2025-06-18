af878e2be3273b893888fceb2b78be0e8f92d554
McCollinsMediaWeb <111267011+McCollinsMediaWeb@users.noreply.github.com>
2025-06-18 13:19:50 +0530

    ddd

--- components/DetailPageComponents/projectdetails44.js
+++ components/DetailPageComponents/projectdetails44.js
@@ -205,12 +205,7 @@ export default function ProjectDetails44({ text1, text2, text3, GalleryImagesWit
         <div className='position-relative pd-common bg2'>
 
             <div className='container'>
-                <motion.div
-                    initial={{ opacity: 0, y: 50 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.8, ease: "easeOut" }}
-                    viewport={{ once: true, amount: 0.5 }} // triggers when 50% of it is in view
-                >
+                
                     <div className='row'>
                         <div className='col-md-12' style={{ textAlign: locale === "ar" ? "right" : undefined }}>
                             <div className="BlT1 nunito-text text-uppercase">{text1}</div>
@@ -285,7 +280,6 @@ export default function ProjectDetails44({ text1, text2, text3, GalleryImagesWit
 
 
                     </div>
-                </motion.div>
 
             </div>
 
