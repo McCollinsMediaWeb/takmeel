337f5bc2540da79a4bcdc4cc21d0ebc6c5f5d032
McCollinsMediaWeb <111267011+McCollinsMediaWeb@users.noreply.github.com>
2025-06-19 18:58:30 +0530

    555

--- app/(home)/detail-page/takmeel-al-barari-view-properties/TakmeelAlBarrariViewClient.js
+++ app/(home)/detail-page/takmeel-al-barari-view-properties/TakmeelAlBarrariViewClient.js
@@ -450,7 +450,7 @@ export default function TakmeelAlBarariViewClient() {
             {/* <div className="FxdHgtImage">
                 <ProjectDetails44  {...DataProjectDetails4} />
             </div> */}
-            <ProjectDetails7 mapimage="mapnew1.jpg" mobileMapImage="mapmobilenew.jpg" {...DataProjectDetails7} />
+            <ProjectDetails7 mapimage="r2.jpg" mobileMapImage="mapmobilenew.jpg" {...DataProjectDetails7} />
             {/* <ProjectDetails3 mainimage1="ak2.jpg" data={DataProjectDetails3} /> */}
             {/* <ProjectDetails5 panoramaimage="/albararistreet.jpg" /> */}
             {/* <ProjectDetails7 mapimage="mapalb.jpg" {...DataProjectDetails7} /> */}
