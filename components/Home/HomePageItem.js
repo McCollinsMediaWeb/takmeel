b80271146abcab62c44a484611f91cf12b72166d
sahadMccollins <sahad@mccollinsmedia.com>
2025-06-25 10:22:18 +0530

    added dubai south section

--- components/Home/HomePageItem.js
+++ components/Home/HomePageItem.js
@@ -148,6 +148,23 @@ export default function HomePageItem() {
       </section>
 
       <section
+        // ref={(el) => (panelsRef.current[1] = el)}
+        className='pageSection'
+      >
+        <ProjectItem
+          backgroundImage="Takmeel-Al-Barrari-View/Majan 03.jpg"
+          backgroundImageMobile="Takmeel-Al-Barrari-View/Mobile Majan 03.jpg"
+          text1={t('slider9.preTitle')}
+          text2={t('slider9.title')}
+          // text3={t('slider9.subTitle')}
+          tagline={t('slider9.content')}
+          url="takmeel-al-barari-view-properties"
+          backgroundVideo="dubai-south.mp4"
+          placeholderImage="dubai-south-cover.png"
+        />
+      </section>
+
+      <section
         // ref={(el) => (panelsRef.current[2] = el)}
         className='pageSection'
       >
